f233b87f0b4986a7f3082d092a77c1138bc83807
Junli Zhang <zhjl86@gmail.com>
2021-02-04 08:11:58 -0800

    simplify cmds

--- cmds.sh
+++ cmds.sh
@@ -4,10 +4,12 @@
 3. Samtools problem: samtools sort did not sort (may be the print is not synchronous), so the sorted bam cannot be indexed. Good thing is it can use the -o option to write to disk
 4. BWA problem: bwa mem not working, only process the reference but not the fastq files; bwa aln -f to file not working and I cannot redirec the binary output to files. Seems the stdout of the filesystme only print text well.
 
-
+### update:
+1. Everything works after removing pthread and changing output file name as an argument instead of "-o" flag.
+2. I also only used the default file system MEMFS (https://emscripten.org/docs/api_reference/Filesystem-API.html), which allows both writing and reading. The WORKERFS file system used originally by Aioli provides read-only access to File and Blob objects, so I cannot make new files there.
 
 ##### samtools
-# samtools sort: give unsorted output possibly due to the asyn javascript writing.    
+
 let samtools = new Aioli("samtools/latest");
 samtools
 .init()
@@ -23,104 +25,46 @@ samtools.ls("/samtools/examples").then(d => console.log(d))
 bwa.ls("/bwa2/examples").then(d => console.log(d))
 
 # check file content with cat
-samtools.ls("/samtools/examples/toy.fa").then(d => console.log(d))
-
-# check whether -o option works
-samtools.exec("view -Sb /samtools/examples/toy.sam -o /samtools/examples/toy.bam")
-    .then((d) => console.log(d.stdout, "ERRRRRRR", d.stderr));
-
-## check bwa mem again
-bwa.exec("index /bwa2/examples/references.fa").then(() => bwa.ls("/bwa2/example")).then(d => console.log(d))
-
-bwa.exec("mem -o /bwa2/example/out2.sam /bwa2/example/references.fa /bwa2/example/2_R1_001.fastq.gz /bwa2/example/2_R2_001.fastq.gz")
-    .then((d) => console.log(d.stdout, "ERRRRR", d.stderr));
-
-
-bwa.exec("mem -o /bwa2/example/out1.sam /bwa2/example/references.fa /bwa2/example/1_R1_001.fastq.gz /bwa2/example/1_R2_001.fastq.gz ")
-    .then((d) => console.log(d.stdout, "ERRRRR", d.stderr));
+samtools.cat("/samtools/examples/toy.fa").then(console.log)
 
 
 
-
-bwa.exec("mem /bwa2/example/references.fa /data/2_R1_001.fastq.gz /data/2_R2_001.fastq.gz")
-    .then((d) => console.log(d.stdout, "ERRRRR", d.stderr));
-
-
-bwa.exec("mem /data/references.fa /data/1_R1_001.fastq.gz /data/1_R2_001.fastq.gz")
-    .then((d) => console.log(d.stdout, "ERRRRR", d.stderr));
-
-
-
-## test samtools
+## sam to bam
 samtools.ls("/samtools/examples").then(d => console.log(d))
+# I modified samtools sort to use the 2nd argument as output name without -o flag, which did not work well.
+#samtools.exec("sort /samtools/examples/1.sam -o /samtools/examples/out1.bam").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
 samtools.exec("sort /samtools/examples/1.sam -o /samtools/examples/out1.bam").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
 
 samtools.ls("/samtools/examples").then(d => console.log(d))
 
-samtools.exec("index /samtools/examples/out1.bam /data/out1.bam.bai").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
+samtools.exec("index /samtools/examples/out1.bam").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
 
-samtools.exec("index /data/out2.bam /samtools/examples/out2.bam.bai").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
-
-samtools.downloadBinary("/samtools/examples/out2.bam.bai").then(d => saveAs(d, "download.bam"));
+# download
 samtools.downloadBinary("/samtools/examples/1.sorted.bam").then(d => saveAs(d, "download.bam"));
 
 
-### load data manually
+### manually loaded data are in /data folder
+# if you use WORKERFS file system, you need write your results into the preloaded folder, here is /samtools/examples
 samtools.ls("/data").then(console.log)
-samtools.exec("sort /data/out2.sam -o /samtools/examples/out2.bam").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
-samtools.ls("/samtools/examples").then(console.log)
-
-samtools.exec("index /samtools/examples/out2.bam").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
-
-samtools.exec("index /data/out2.bam /samtools/examples/out2.bam.bai").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
-
-samtools.downloadBinary("1.bam").then(d => saveAs(d, "out1.bam"));
-
-##
-samtools.ls("/data").then(console.log)
-samtools.exec("sort /data/out2.sam -o /data/out2.bam").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
-samtools.ls("/samtools/examples").then(console.log)
-
+#samtools.exec("sort /data/out2.sam /samtools/examples/out2.bam").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
+#samtools.exec("index /samtools/examples/out2.bam").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
+#samtools.exec("index /data/out2.bam /samtools/examples/out2.bam.bai").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
+samtools.exec("sort /data/out2.sam /data/out2.bam").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
+samtools.exec("index /data/out2.bam").then(d => console.log(d.stdout, "ERRRRRRR", d.stderr));
+samtools.downloadBinary("out2.bam").then(d => saveAs(d, "out2.bam"));
 
 ## test bwa
-bwa.ls("/bwa2/examples").then(d => console.log(d))
-bwa.exec("index /bwa2/examples/references.fa").then((d) => console.log(d.stdout, "ERRRRR", d.stderr)).then(() => bwa.ls("/bwa2/examples")).then(d => console.log(d))
-
-bwa.exec("mem /bwa2/examples/references.fa /bwa2/examples/2_S2_L001_R1_001.fastq.gz /bwa2/examples/2_S2_L001_R2_001.fastq.gz")
+bwa.ls("/data").then(d => console.log(d))
+bwa.exec("index /data/references.fa").then((d) => console.log(d.stdout, "ERRRRR", d.stderr)).then(() => bwa.ls("/data")).then(d => console.log(d))
+# no need -o, directly give the output name after the 2nd fastq file
+bwa.exec("mem /data/references.fa /data/2_S2_L001_R1_001.fastq.gz /data/2_S2_L001_R2_001.fastq.gz /data/test.sam")
     .then((d) => console.log(d.stdout, "ERRRRR", d.stderr));
-
-bwa.exec("mem -o /data/test.sam /bwa2/examples/references.fa /bwa2/examples/2_S2_L001_R1_001.fastq.gz /bwa2/examples/2_S2_L001_R2_001.fastq.gz")
-    .then((d) => console.log(d.stdout, "ERRRRR", d.stderr));
-
-
-
-
-
-
-bwa.exec("mem /bwa2/examples/references.fa /bwa2/examples/2_S2_L001_R1_001.fastq.gz /bwa2/examples/2_S2_L001_R2_001.fastq.gz")
-    .then(d => {
-        console.log(d.stderr, "\nEnd of stderr");
-        let file = new stdInfo("/bwa2/examples/R1.sai", d.stdout);
-        return file;
-    })
-    .then(d => bwa.write(d));
-    
-bwa.downloadText("/bwa2/examples/R1.sai").then(d => saveAs(d, "R1.sam"));
-
-
 
 ## set working environment
-bwa.setwd("/bwa2/examples/").then(d => console.log(d));
+bwa.setwd("/data").then(d => console.log(d));
 bwa.exec("index references.fa").then((d) => console.log(d.stdout, "ERRRRR", d.stderr)).then(() => bwa.ls("/bwa2/examples")).then(d => console.log(d));
-bwa.exec("mem -o out2.sam references.fa 2_S2_L001_R1_001.fastq.gz 2_S2_L001_R2_001.fastq.gz")
+bwa.exec("mem references.fa 2_S2_L001_R1_001.fastq.gz 2_S2_L001_R2_001.fastq.gz out2.sam")
     .then((d) => console.log(d.stdout, "ERRRRR", d.stderr));
-
-bwa.exec("aln -o R1.sai  references.fa 2_S2_L001_R1_001.fastq.gz")
-    .then((d) => console.log(d.stdout, "ERRRRR\n", d.stderr));
-
-
-
-
 bwa.ls("/data").then(console.log)
 
 
