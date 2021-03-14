3a84657aa4c8a90f54f1e5bd64a7b7fa5b572844
Junli Zhang <zhjl86@gmail.com>
2021-03-14 13:41:46 -0700

    add patch files for samtools and bwa

--- Tool_src/bwa-compile.sh
+++ Tool_src/bwa-compile.sh
@@ -12,7 +12,9 @@ emmake make
 # add the 4th argument as output filename for bwa mem
 emcc -O2 bwa2.o -o ../build/bwa2.html -s FORCE_FILESYSTEM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=["callMain"] -s ALLOW_MEMORY_GROWTH=1 -lworkerfs.js --preload-file examples/@/bwa2/examples/
 
-
+# make patch
+# git diff old-git new-git folder
+git diff 122146e0086857630d40e216a21280711482d899 f80267ae13be4b9e1b8462f8907858182664ccf7 bwa-0.7.17/ > bwa.patch.txt
 
 
 
--- Tool_src/samtools-compile.sh
+++ Tool_src/samtools-compile.sh
@@ -13,3 +13,7 @@ emconfigure ./configure --without-curses CFLAGS="-g -Wall -O2 -s USE_ZLIB=1 -s U
 # kept only index and sort functions in bamtk.c to reduce size
 emmake make CC=emcc AR=emar CFLAGS="-g -Wall -O2"
 emcc -O2 samtools.o -o ./build/samtools.html -s FORCE_FILESYSTEM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=["callMain"] -s ALLOW_MEMORY_GROWTH=1 -s ERROR_ON_UNDEFINED_SYMBOLS=0 -lworkerfs.js --preload-file examples/@/samtools/examples/
+
+# make patch
+# git diff old-git new-git folder
+git diff 122146e0086857630d40e216a21280711482d899 f80267ae13be4b9e1b8462f8907858182664ccf7 samtools-1.11/ > samtools.patch.txt
