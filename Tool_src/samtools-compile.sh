b8ceed46f2bcc562c72f04b821aee0623b0b49e3
Junli Zhang <zhjl86@gmail.com>
2022-04-09 18:56:11 -0700

    add samtools depth

--- Tool_src/samtools-compile.sh
+++ Tool_src/samtools-compile.sh
@@ -11,7 +11,7 @@ emconfigure ./configure --without-curses CFLAGS="-g -Wall -O2 -s USE_ZLIB=1 -s U
 
 ## Remove pthread_create functions in bam_sort.c
 # kept only index and sort functions in bamtk.c to reduce size
-emmake make CC=emcc AR=emar CFLAGS="-g -Wall -O2"
+emmake make CC=emcc AR=emar CFLAGS="-g -Wall -O2 -s USE_ZLIB=1 -s USE_BZIP2=1"
 emcc -O2 samtools.o -o ./build/samtools.html -s FORCE_FILESYSTEM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=["callMain"] -s ALLOW_MEMORY_GROWTH=1 -s ERROR_ON_UNDEFINED_SYMBOLS=0 -lworkerfs.js --preload-file examples/@/samtools/examples/
 
 # make patch
