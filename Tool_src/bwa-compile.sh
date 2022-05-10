94a4807d53d01993c77dfbb00fc05b3cbef64efe
Junli Zhang <zhjl86@gmail.com>
2022-05-10 13:50:28 -0700

    update patch file

--- Tool_src/bwa-compile.sh
+++ Tool_src/bwa-compile.sh
@@ -10,12 +10,13 @@ emmake make
 ## seems emscripten did not work well with pthread, so I removed all the pthread functions in fastmap.c and pthread.c
 # to make bwa mem work
 # add the 4th argument as output filename for bwa mem
-emcc -O2 bwa2.o -o ../build/bwa2.html -s FORCE_FILESYSTEM=1 -s EXTRA_EXPORTED_RUNTIME_METHODS=["callMain"] -s ALLOW_MEMORY_GROWTH=1 -lworkerfs.js --preload-file examples/@/bwa2/examples/
+emcc -O2 bwa2.o -o ../build/bwa2.html -s FORCE_FILESYSTEM=1 -s EXPORTED_RUNTIME_METHODS=["callMain"] -s ALLOW_MEMORY_GROWTH=1 -lworkerfs.js --preload-file examples/@/bwa2/examples/
 
 # make patch
 # git diff old-git new-git folder
-git diff 122146e0086857630d40e216a21280711482d899 305ac46b82484635bf3d845c7adee31c37692245 bwa-0.7.17/ > bwa.patch.txt
+git diff 122146e0086857630d40e216a21280711482d899 da7615d98affd2567f49a098614509266493683b bwa-0.7.17/ > bwa.patch.txt
 
 
 
-
+# update:
+- 2022-05-10 add exactSNP as `bwa call` and editcall as `bwa editcall`
