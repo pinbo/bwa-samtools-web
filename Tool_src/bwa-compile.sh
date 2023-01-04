cc10acaaa20c85802c10eff0a933730adf915e3f
Junli Zhang <zhjl86@gmail.com>
2023-01-03 21:44:16 -0800

    update bwa compile

--- Tool_src/bwa-compile.sh
+++ Tool_src/bwa-compile.sh
@@ -14,9 +14,11 @@ emmake make
 emcc -O2 bwa2.o -o bwa2.js -s FORCE_FILESYSTEM=1 -s EXPORTED_RUNTIME_METHODS=["callMain"] -s ALLOW_MEMORY_GROWTH=1
 # make patch
 # git diff old-git new-git folder
+git log .
 git diff 122146e0086857630d40e216a21280711482d899 da7615d98affd2567f49a098614509266493683b bwa-0.7.17/ > bwa.patch.txt
 
 
 
 # update:
-- 2022-05-10 add exactSNP as `bwa call` and editcall as `bwa editcall`
+- 2023-01-03: add mate mapping quality (MQ) in the output
+- 2022-05-10: add exactSNP as `bwa call` and editcall as `bwa editcall`
