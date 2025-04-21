f5c9b783595b42cbd0c33323c0b3178b7691db87
Junli Zhang <zhjl86@gmail.com>
2025-04-20 21:06:12 -0700

    update gzip patch and compile script

--- Tool_src/gzip-compile.sh
+++ Tool_src/gzip-compile.sh
@@ -1,6 +1,12 @@
+# ./emsdk install latest (v '4.0.7' works)
+# ./emsdk activate latest
+
 # 1. Download gzip 1.12 from here
 wget https://ftp.gnu.org/gnu/gzip/gzip-1.12.zip
 # 2. unzip and cd the folder
+unzip gzip-1.12.zip
 cd gzip-1.12
-emconfigure ./configure --disable-year2038
+emconfigure ./configure # --disable-year2038 # emsdk v4.0.7 has no errors without disabling year2038.
 # modify the Makefile and the gzip.c based on the patch file.
+# the patch file line number dependes on the emsdk version.
+emmake make
